358a14c1234723016603e393172544c0be30f438
Sam Patterson <145644525+SamP521@users.noreply.github.com>
2024-07-11 16:15:22 -0400

    Updated arithmetic-formatter.py
    Removed unused variables, converted unnecessary lambda function to regular

--- arithmetic-formatter/arithmetic_formatter.py
+++ arithmetic-formatter/arithmetic_formatter.py
@@ -5,36 +5,31 @@ def main():
 def arithmetic_arranger(problems, show_answers=False):
     if len(problems) > 5:
         return "Error: Too many problems."
-    
+
     problem_list = [equation.split(' ') for equation in problems]
     operators = ["+", "-"]
-    answer_list = []
-    size_list = [max(len(equation[0]), len(equation[2])) + 2 for equation in problem_list]
     line1 = []
     line2 = []
     dashes = []
     results = []
-    eq_length = lambda equation: max(len(equation[0]), len(equation[2])) + 2
 
-    
     for equation in problem_list:
         first = equation[0]
         second = equation[2]
         operator = equation[1]
 
-        # Error Checks
         if operator not in operators:
             return "Error: Operator must be '+' or '-'."
         if not first.isdigit() or not second.isdigit():
             return "Error: Numbers must only contain digits."
         if len(first) > 4 or len(second) > 4:
             return "Error: Numbers cannot be more than four digits."
-        
+
         if operator == '+':
             result = str(int(first) + int(second))
         else:
             result = str(int(first) - int(second))
-        
+
         line1.append(" " * (eq_length(equation) - len(first)) + first)
         line2.append(operator + " " * (eq_length(equation) - len(second) - 1) + second)
         dashes.append("-" * eq_length(equation))
@@ -47,8 +42,11 @@ def arithmetic_arranger(problems, show_answers=False):
 
     if show_answers:
         return "\n".join([adj_line1, adj_line2, adj_dashes, adj_results])
-    else:
-        return "\n".join([adj_line1, adj_line2, adj_dashes])
+
+    return "\n".join([adj_line1, adj_line2, adj_dashes])
+
+def eq_length(equation):
+    return max(len(equation[0]), len(equation[2])) + 2
 
 if __name__ == "__main__":
     main()
