86472f6ac3525631b3f8325e73dd355dc2b21798
Sam Patterson <145644525+SamP521@users.noreply.github.com>
2024-07-11 16:13:28 -0400

    Updated test formats
    Decreased line length and removed extraneous pytest import

--- arithmetic-formatter/test_arithmetic_formatter.py
+++ arithmetic-formatter/test_arithmetic_formatter.py
@@ -1,26 +1,37 @@
 from arithmetic_formatter import arithmetic_arranger
-import pytest
 
 
 def test_standard():
-    assert arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]) == "    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----"
+    problems = ["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]
+    expected = "    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----"
+    assert arithmetic_arranger(problems) == expected
 
 
 def test_too_many_problems():
-    assert arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "988 + 40", "1 - 9380"]) == "Error: Too many problems."
+    problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "988 + 40", "1 - 9380"]
+    expected = "Error: Too many problems."
+    assert arithmetic_arranger(problems) == expected
 
 
 def test_invalid_operator():
-    assert arithmetic_arranger(["3 / 855", "3801 - 2", "45 + 43", "123 + 49"]) == "Error: Operator must be '+' or '-'."
+    problems = ["3 / 855", "3801 - 2", "45 + 43", "123 + 49"]
+    expected = "Error: Operator must be '+' or '-'."
+    assert arithmetic_arranger(problems) == expected
 
 
 def test_too_many_digits():
-    assert arithmetic_arranger(["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"]) == "Error: Numbers cannot be more than four digits."
+    problems = ["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"]
+    expected = "Error: Numbers cannot be more than four digits."
+    assert arithmetic_arranger(problems) == expected
 
 
 def test_only_digits():
-    assert arithmetic_arranger(["98 + 3g8", "3801 - 2", "45 + 43", "123 + 49"]) == "Error: Numbers must only contain digits."
+    problems = ["98 + 3g8", "3801 - 2", "45 + 43", "123 + 49"]
+    expected = "Error: Numbers must only contain digits."
+    assert arithmetic_arranger(problems) == expected
 
 
 def test_show_answers():
-    assert arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True) == "   32         1      45      123      988\n- 698    - 3801    + 43    +  49    +  40\n-----    ------    ----    -----    -----\n -666     -3800      88      172     1028"
+    problems = ["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"]
+    expected = "   32         1      45      123      988\n- 698    - 3801    + 43    +  49    +  40\n-----    ------    ----    -----    -----\n -666     -3800      88      172     1028"
+    assert arithmetic_arranger(problems, True) == expected
